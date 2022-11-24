5104ff930ee6538e4aa1b2993aec0fe2442b2aac
Joao Paulo <oliveirajpufrj@gmail.com>
2022-11-23 23:47:54 -0300

    feat: get clinicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {HttpClientModule} from "@angular/common/http"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ClinicasService } from 'src/app/services/clinicas.service';
+import { Clinica } from 'src/app/types/Clinica';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  clinicas: Clinica[] = []
+
+  constructor(private clinicasService: ClinicasService) { 
+    this.getClinicas();
+  }
+
+  ngOnInit(): void {
+  }
+
+  getClinicas(): void {
+    this.clinicasService.getAll().subscribe((clinicasResponse) => this.clinicas = clinicasResponse);
+  }
+
+}
